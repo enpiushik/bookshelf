62f3fd422d5183ff594af63ca7fc6422cd76b0aa
java_tsi08 <enpiushik@inbox.lv>
2019-01-24 20:56:48 +0200

    lesson 24.01.2019

--- src/main/java/lv/tsi/javacourses/bookshelf/books/boundary/ManageBooksBean.java
+++ src/main/java/lv/tsi/javacourses/bookshelf/books/boundary/ManageBooksBean.java
@@ -1,13 +1,13 @@
 package lv.tsi.javacourses.bookshelf.books.boundary;
 
-import lv.tsi.javacourses.bookshelf.auth.boundary.CurrentUser;
 import lv.tsi.javacourses.bookshelf.books.model.ReservationEntity;
+import lv.tsi.javacourses.bookshelf.books.model.ReservationStatus;
 
 import javax.faces.view.ViewScoped;
-import javax.inject.Inject;
 import javax.inject.Named;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
@@ -15,22 +15,17 @@ import java.util.Optional;
 
 @Named
 @ViewScoped
-public class MyBooksBean implements Serializable {
+public class ManageBooksBean implements Serializable {
     @PersistenceContext
     private EntityManager em;
-    @Inject
-    private CurrentUser currentUser;
     private List<ReservationEntity> availableResult;
-    private List<ReservationEntity> inQueueResult;
     private List<ReservationEntity> takenResult;
     private List<ReservationEntity> closedResult;
 
     public void prepare() {
         availableResult = new ArrayList<>();
-        inQueueResult = new ArrayList<>();
         closedResult = new ArrayList<>();
-        List<ReservationEntity> userReservations = em.createQuery("select r from Reservation r where r.user = :user and r.status = 'ACTIVE'", ReservationEntity.class)
-                .setParameter("user", currentUser.getUser())
+        List<ReservationEntity> userReservations = em.createQuery("select r from Reservation r where r.status = 'ACTIVE'", ReservationEntity.class)
                 .getResultList();
 
         for (ReservationEntity r : userReservations) {
@@ -39,28 +34,34 @@ public class MyBooksBean implements Serializable {
                     .setParameter("book", r.getBook())
                     .getResultStream()
                     .findFirst();
-
             if (firstReservation.isEmpty() || firstReservation.get().getId().equals(reservationId)) {
                 availableResult.add(r);
-            } else {
-                inQueueResult.add(r);
             }
         }
-        takenResult = em.createQuery("select r from Reservation r where r.user = :user and r.status = 'TAKEN'", ReservationEntity.class)
-                .setParameter("user", currentUser.getUser())
+
+        takenResult = em.createQuery("select r from Reservation r where r.status = 'TAKEN'", ReservationEntity.class)
                 .getResultList();
 
-        closedResult = em.createQuery("select distinct r from Reservation r where r.user = :user and r.status = 'CLOSED'", ReservationEntity.class)
-                .setParameter("user", currentUser.getUser())
+        closedResult = em.createQuery("select r from Reservation r where r.status = 'CLOSED'", ReservationEntity.class)
                 .getResultList();
+    }
+
+    @Transactional
+    public void giveBook(ReservationEntity reservation) {
+        ReservationEntity r = em.merge(reservation);
+        r.setStatus(ReservationStatus.TAKEN);
+        prepare();
+    }
+
+    @Transactional
+    public void takeBook(ReservationEntity reservation) {
+        ReservationEntity r = em.merge(reservation);
+        r.setStatus(ReservationStatus.CLOSED);
+        prepare();
     }
 
     public List<ReservationEntity> getAvailableBooks() {
         return availableResult;
-    }
-
-    public List<ReservationEntity> getInQueueBooks() {
-        return inQueueResult;
     }
 
     public List<ReservationEntity> getTakenBooks() {
